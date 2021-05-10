74fc4ba4a9b9742efe2c636da63f23a8b06cb22c
Everton P <evertonportela@outlook.com>
2021-05-09 22:34:44 -0300

    commit initial

---
+++ quemsomos.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>Quem Somos</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+    <!-- Tweaks for older IEs-->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <!-- owl stylesheets -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
+</head>
+<!-- body -->
+
+<body class="about_page">
+
+    </div>
+    <?php
+    require "menu.php";
+    ?>
+    <!-- end header end -->
+    <!--services start -->
+    <div class="about_main">
+        <div class="services_main">
+            <div class="container">
+                <div class="creative_taital">
+                    <h1 class="creative_text">Quem somos</h1>
+                    <p style="color: #050000; text-align: center;">É um fato estabelecido há muito tempo que um leitor se distrairá com o conteúdo legível de uma página ao examinar seu layout. O objetivo de usar Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, ao contrário de usar 'Conteúdo aqui, conteúdo aqui', fazendo com que pareça um inglês legível. Muitos pacotes de editoração eletrônica e editores de páginas da web agora usam Lorem Ipsum como seu texto de modelo padrão, e uma pesquisa por 'lorem ipsum' revelará muitos sites ainda em sua infância. É um fato estabelecido há muito tempo que um leitor se distrairá com o conteúdo legível de uma página ao observar seu layout. O objetivo de usar Lorem Ipsum é que ele tem uma distribuição de letras mais ou menos normal, ao contrário de usar 'Conteúdo aqui, conteúdo aqui', fazendo com que pareça um inglês legível.</p>
+                    <!-- <div class="btn_main">
+                        <button type="button" class="btn btn-dark btn-lg">Get started</button>
+                    </div> -->
+                </div>
+            </div>
+
+            <!--Contact_section start -->
+            <div class="contact_main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1 class="touch_text">Contate-Nos</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact_section_2">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="map_icon">
+                                    <img src="images/map-icon.png" style="max-width: 100%;padding-left: 30px; ">
+                                    <p class="email-text"><a href="#">610, R. João Café Filho, 264, Estância - SE, 49200-000<br></a></p>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="map_icon">
+                                    <img src="images/call-icon.png" style="max-width: 100%;padding-left: 30px;">
+                                    <p class="email-text"><a href="#">79 99848-0217</a></p>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="map_icon">
+                                    <img src="images/email-icon.png" style="max-width: 100%; padding-left: 30px;">
+                                    <p class="email-text"><a href="#">redeinfo.tuco@gmail.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Contact_section end -->
+                <div class="copyright">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <p class="copyright_text">© 2019 Todos os direitos reservados.<a href="https://html.design">rede infos tucos</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Javascript files-->
+                <script src="js/jquery.min.js"></script>
+                <script src="js/popper.min.js"></script>
+                <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
